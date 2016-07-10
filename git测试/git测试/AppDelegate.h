37247ffabecb5852b65fced7f18467b6211ea5c1
李 恒 <lh@sumomomatoMacBook-Pro.local>
2016-07-10 21:55:34 +0800

    first

---
+++ git测试/git测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git测试
+//
+//  Created by 李 恒 on 16/7/10.
+//  Copyright © 2016年 李 恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git测试/git测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git测试
+//
+//  Created by 李 恒 on 16/7/10.
+//  Copyright © 2016年 李 恒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
